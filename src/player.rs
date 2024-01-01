31623ee4f425d206a24dcb5ac9befbbf43e567e7
Romain LE JEUNE <romain.le.jeune@proton.ch>
2024-01-01 08:41:46 +0100

    feat: init player creation.

---
+++ src/player.rs
@@ -0,0 +1,19 @@
+use crate::coordinate::Coordinate;
+
+enum Status{
+    Idle,
+    Pending,
+    Dead
+}
+pub struct Player {
+    name: String,
+    coord: Coordinate,
+    score: u32,
+    status: Status
+}
+
+impl Player {
+    pub fn new(name:String) -> Player {
+        Player{name:name, coord:Coordinate::new(0, 0), score:0, status:Status::Idle}
+    }
+}
